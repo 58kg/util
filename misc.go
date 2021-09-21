0043279b6b906d1c74f3131038a3eed6a7d8fc7f
58kg <unknow>
2021-09-21 23:59:04 +0800

    [feat] to_string && retry
    Change-Id: I0c04d4d838f05e14638c4177118c5c8039436622

---
+++ misc.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// RetryDo Retry sn表示这是第几次执行f, 至多执行times次f
+func Retry(f func(sn int) (end bool), times int, maxSleepTime time.Duration) {
+	for i := 1; i <= times; i++ {
+		if f(i) || i == times {
+			return
+		}
+		time.Sleep(time.Duration(random.Int63n(int64(maxSleepTime))))
+	}
+}
