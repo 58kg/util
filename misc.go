68fc7e83250f00d36aedc4b47fed06958101c07c
58kg <unknow>
2021-11-11 17:01:54 +0800

    [feat] 添加ExternalIP
    Change-Id: I2f843b02e3b9eae85752fda19a56cd5c6e09d754

--- misc.go
+++ misc.go
@@ -1,7 +1,9 @@
 package util
 
 import (
+	"errors"
 	"math/rand"
+	"net"
 	"regexp"
 	"time"
 )
@@ -22,4 +24,49 @@ var trimWhiteReg = regexp.MustCompile(`(^\s+)|(\s+$)`)
 
 func TrimWhite(s string) string {
 	return trimWhiteReg.ReplaceAllString(s, "")
+}
+
+func ExternalIP() (net.IP, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			ip := getIpFromAddr(addr)
+			if ip == nil {
+				continue
+			}
+			return ip, nil
+		}
+	}
+	return nil, errors.New("may be not connect to the network")
+}
+
+func getIpFromAddr(addr net.Addr) net.IP {
+	var ip net.IP
+	switch v := addr.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	if ip == nil || ip.IsLoopback() {
+		return nil
+	}
+	ip = ip.To4()
+	if ip == nil {
+		return nil
+	}
+	return ip
 }
